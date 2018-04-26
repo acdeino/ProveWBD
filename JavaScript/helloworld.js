06fcd6a06f9eab7ffc2df8518799b2f278eb67c0
namlose <ahbehnomics@gmail.com>
2018-04-26 20:15:52 +0200

    JS

---
+++ JavaScript/helloworld.js
@@ -0,0 +1,30 @@
+document.write("Hello World </br>");
+document.write("My name is Diòscuro");
+// this is a comment
+// document.write("This is a comment");
+
+//Variables //
+//number Variables
+var x =10;
+x=x+5;
+  document.write(x + "<br>");
+/* string variable */
+var name = "Norberto";
+document.write(name);
+/* boolean variable */
+var bool = true; //or = false;
+/* undefined variable. Variable exists, but it hasn't got a value */
+var y;
+var = z undefined;
+/* null variable */
+var = c null;
+/* array Variable */
+var transport = ["car","bus","motorcycle","train","plane"];
+var myobjects = {
+  name="john",
+  age=20
+  address= "Corso Duca degli Abruzzi 29 10129 Torino"
+};
+document.write("<br>"+myobjects.age);
+document.write("<br>"+myobjects.address+"<br>");
+document.write(transport[1]);
