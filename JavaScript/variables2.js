fdd2acc826af4427cab1f8730f22bd1ec4fa52f7
namlose <ahbehnomics@gmail.com>
2018-04-27 03:36:14 +0200

    var

---
+++ JavaScript/variables2.js
@@ -0,0 +1,18 @@
+var x = 10;
+var z = 8;
+j == x + z;
+document.write(j + "<br>");
+
+var name = "Norberto";
+document.write(name+"<br>");
+
+var transport = ["bus","car","plane","train"];
+document.write(transport[2] + "<br>");
+
+var database = [
+  name1 = "John",
+  age = 28,
+  address = "C.so Duca 29, 10129 Torino, Italia"
+];
+document.write(database.age+"<br>");
+document.write(database.address + "<br>");
